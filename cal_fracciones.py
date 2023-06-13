1ed0ca0397c9909ab00c7403768632962864ba7d
Yeremi <yeremi.samuel@gmail.com>
2023-06-13 18:50:32 -0300

    Se agregaron calculadoras faltantes y diagramas de flujo

--- cal_fracciones.py
+++ cal_fracciones.py
@@ -14,8 +14,8 @@ def sumar_fracciones(num1, den1, num2, den2):
     num = (num1 * den2) + (num2 * den1)
     den = den1 * den2
     return simplificar_fraccion(num, den)
- 
-def restar_fracciones(num1, den1, num2, den2): 
+
+def restar_fracciones(num1, den1, num2, den2):
     num = (num1 * den2) - (num2 * den1)
     den = den1 * den2
     return simplificar_fraccion(num, den)
